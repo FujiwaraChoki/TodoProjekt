c74e0889684586ca828c52805d2b0c6178c34ee7
Sami Hindi <lolipop66611@gmail.com>
2022-10-05 21:53:51 +0200

    Filter Button in Dashboard & Functionality

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 const BASE_URL = "http://127.0.0.1:3000/";
+const tasksLists = document.querySelector("#taskTable");
+let filterMethod;
 
+// Method to create a new task
 const createTaskElement = (id, completed, title) => {
       let element = document.createElement("tr");
       let text_to_show = "Due";
@@ -12,9 +15,9 @@ const createTaskElement = (id, completed, title) => {
       return element;
 }
 
-const getTasks = () => {
-      const tasksLists = document.querySelector("div table #taskTable");
-      let url = BASE_URL + "tasks";
+// Method to return all tasks from the local server
+const getTasks = (url_extension) => {
+      const url = `${BASE_URL}${url_extension}`;
       const options = {
             method: "GET",
             headers: new Headers({
@@ -22,17 +25,75 @@ const getTasks = () => {
             })
       };
     
-      fetch(`${url}`, options)
-      .then(response => response.json())
-      .then(data => {
-            data.forEach((task) => {
-                  tasksLists.appendChild(createTaskElement(task.id, task.completed, task.title));
-            });
+      fetch(url, options)
+      .then(response => {
+            return response.json();
+      })
+};
+
+const getCompletedTasks = () => {
+      const completedTasks =  getTasks('tasks').filter(task => {task.completed});
+      completedTasks.forEach((task) => {
+            tasksLists.appendChild(createTaskElement(task.id, task.completed, task.title));
       });
+};
+
+const getUncompletedTasks = () => {
+      const completedTasks =  getTasks('tasks').filter(task => task.completed === false);
+      completedTasks.forEach((task) => {
+            tasksLists.appendChild(createTaskElement(task.id, task.completed, task.title));
+      });
+};
+
+const getAllTasks = () => {
+      const data = getTasks('tasks');
+      data.forEach((task) => {
+            tasksLists.appendChild(createTaskElement(task.id, task.completed, task.title));
+      });
+};
+
+const getSpecificTask = (id) => {
+      const data = getTasks('task');
+      const task = data.filter(task => task.id === id);
+      tasksLists.appendChild(createTaskElement(task.id, task.completed, task.title));
 };
 
 
 document.addEventListener('DOMContentLoaded', () => {
-      let dashboardBody = document.querySelector('#dashboardBody');
-      dashboardBody.addEventListener('load', getTasks());
+      // Display all tasks in all cases once the page loads
+      const dashboardBody = document.querySelector('#dashboardBody');
+      dashboardBody.addEventListener('load', getTasks('tasks'));
+      
+      // Add Event Listener for the Selection of the Filtering method
+      const selectFilterMethod = document.querySelector('#selectFilterMethod');
+      // Add Event Listener for the Filter Button
+      const filterButton = document.querySelector('#filterButton');
+      selectFilterMethod.addEventListener('blur', (event) => {
+            let idField = document.querySelector('#askForTaskID');
+            filterMethod = event.target.value;
+            if(filterMethod === "specificTask") {
+                  // This needs some extra work to get the ID of the task
+                  
+                  idField.classList.remove('hidden');
+            } else {
+                  idField.classList.add('hidden');
+            }
+
+            filterButton.addEventListener(('click'), () => {
+                  switch(filterMethod) {
+                        case "all":
+                              getTasks();
+                              break;
+                        case "done":
+                              getCompletedTasks();
+                              break;
+                        case "due":
+                              getUncompletedTasks();
+                              break;
+                        case "specificTask":
+                              getSpecificTask(idField.value);
+                              break;
+                  }
+            });
+      });
 });
