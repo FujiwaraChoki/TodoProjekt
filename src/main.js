7bed25516b5a5d6426872a787d3c7a0e774ebee1
Sami Hindi <sami@samihindi.com>
2022-10-06 16:29:24 +0200

    Added Music Controls in the Navbar

--- src/main.js
+++ src/main.js
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
       };
 
       // Method to create a new task
-      const createTaskElement = (id, completed, title, deleteButtonTableData) => {
+      const createTaskElement = (id, completed, title, deleteButtonTableData, updateButton) => {
             let element = document.createElement("tr");
             let text_to_show = 'Due';
             if (completed === 'true') {
@@ -35,6 +35,7 @@ document.addEventListener('DOMContentLoaded', () => {
             <td style="margin-top: 2%;">${title}</td>
             <td style="margin-top: 2%;">${text_to_show}</td>`;
             element.appendChild(deleteButtonTableData);
+            element.appendChild(updateButton);
             return element;
       };
 
@@ -64,8 +65,22 @@ document.addEventListener('DOMContentLoaded', () => {
             });
       };
 
+      const updateTask = (id, new_title) => {
+            let url = `${BASE_URL}tasks`;
+            
+            const data = {
+                  id: id,
+                  title: new_title
+            };
+
+            options.method = 'PUT';
+            options.body = JSON.stringify(data);
+            fetch(url, options);
+      };
+
       // Method to get completed Tasks
       const getCompletedTasks = () => {
+            clean(tasksList);
             let url = `${BASE_URL}auth/cookie/tasks`;
 
             fetch(url, options)
@@ -81,9 +96,16 @@ document.addEventListener('DOMContentLoaded', () => {
                                     deleteButton.addEventListener('click', () => {
                                           deleteTask(task.id);
                                     });
-                                    tasksList.appendChild(deleteButton);
-                                    tasksList.appendChild(createTaskElement(task.id, task.completed, task.title));
-                              });
+                                    let updateButton = document.createElement("button");
+                                    updateButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16"><path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/><path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/></svg>'
+                                    updateButton.style.marginTop = '2%';
+                                    updateButton.style.marginLeft = '2%';
+                                    updateButton.setAttribute('class', 'btn btn-primary');
+                                    updateButton.setAttribute('scope', 'row');
+                                    updateButton.addEventListener('click', () => {
+                                          updateTask(task.id, prompt('Enter a new title for the task'));
+                                    });
+                                    tasksList.appendChild(createTaskElement(task.id, task.completed, task.title, deleteButton, updateButton));                              });
                         }
                   });
 
@@ -91,6 +113,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
       // Method to get uncompleted Tasks
       const getUncompletedTasks = () => {
+            clean(tasksList);
             let url = `${BASE_URL}auth/cookie/tasks`;
 
             fetch(url, options)
@@ -102,19 +125,33 @@ document.addEventListener('DOMContentLoaded', () => {
                         });
 
                         uncompletedTasks.forEach((task) => {
-                              const deleteButton = document.createElement("td");
-                              deleteButton.innerHTML = "<button scope='row' class='btn btn-danger'>Delete</button>";
+                              let deleteButton = document.createElement("button");
+                              deleteButton.innerHTML = "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash' viewBox='0 0 16 16'><path d='M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z'/><path fill-rule='evenodd' d='M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z'/></svg>";
+                              deleteButton.setAttribute('class', 'btn btn-danger');
+                              deleteButton.setAttribute('scope', 'row');
                               deleteButton.addEventListener('click', () => {
                                     deleteTask(task.id);
                               });
-                              tasksList.appendChild(deleteButton);
-                              tasksList.appendChild(createTaskElement(task.id, task.completed, task.title));
+                              deleteButton.style.marginTop = '2%';
+                              deleteButton.style.marginLeft = '2%';
+                              
+                              let updateButton = document.createElement("button");
+                              updateButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16"><path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/><path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/></svg>'
+                              updateButton.style.marginTop = '2%';
+                              updateButton.style.marginLeft = '2%';
+                              updateButton.setAttribute('class', 'btn btn-primary');
+                              updateButton.setAttribute('scope', 'row');
+                              updateButton.addEventListener('click', () => {
+                                    updateTask(task.id, prompt('Enter a new title for the task'));
+                              });
+                              tasksList.appendChild(createTaskElement(task.id, task.completed, task.title, deleteButton, updateButton));
                         });
                   });
       };
 
       // Method to get all Tasks
       const getAllTasks = () => {
+            clean(tasksList);
             let url = `${BASE_URL}auth/cookie/tasks`;
 
             fetch(url, options)
@@ -130,7 +167,16 @@ document.addEventListener('DOMContentLoaded', () => {
                               });
                               deleteButton.style.marginTop = '2%';
                               deleteButton.style.marginLeft = '2%';
-                              tasksList.appendChild(createTaskElement(task.id, task.completed, task.title, deleteButton));
+                              let updateButton = document.createElement("button");
+                              updateButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16"><path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/><path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/></svg>'
+                              updateButton.addEventListener('click', () => {
+                                    updateTask(task.id, prompt('Enter a new title for the task'));
+                              });
+                              updateButton.style.marginTop = '2%';
+                              updateButton.style.marginLeft = '2%';
+                              updateButton.setAttribute('class', 'btn btn-primary');
+                              updateButton.setAttribute('scope', 'row');
+                              tasksList.appendChild(createTaskElement(task.id, task.completed, task.title, deleteButton, updateButton));
                         });
                   });
       };
@@ -226,7 +272,8 @@ document.addEventListener('DOMContentLoaded', () => {
                         }
                   });
             });
-            // method: createTask
+
+            // Method: createTask
             const createTaskButton = document.querySelector('#createTaskButton');
             const taskTitle = document.querySelector('#taskTitle');
             createTaskButton.addEventListener('click', () => {
